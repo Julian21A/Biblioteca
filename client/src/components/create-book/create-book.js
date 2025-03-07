f5368d923c626ea7e681a242baa72cc083ca6279
Julian21A <cj.avcj1@gmail.com>
2025-03-07 10:28:15 -0500

    se crea Menu desplegable

---
+++ client/src/components/create-book/create-book.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./create-book.css"
+
+export function CreateBook() {
+    return(
+        <div>
+            soy un libro
+        </div>
+    )
+};
+
+export default CreateBook;
