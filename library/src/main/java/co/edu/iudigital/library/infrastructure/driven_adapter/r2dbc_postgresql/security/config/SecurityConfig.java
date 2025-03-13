86410f03e218f74aa6b82f0d889bd14f997292d4
DarkDev <redprogramador@gmail.com>
2025-03-12 20:19:04 -0500

    update config

--- library/src/main/java/co/edu/iudigital/library/infrastructure/driven_adapter/r2dbc_postgresql/security/config/SecurityConfig.java
+++ library/src/main/java/co/edu/iudigital/library/infrastructure/driven_adapter/r2dbc_postgresql/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -41,8 +42,10 @@ public class SecurityConfig {
                     config.setAllowCredentials(true);
                     return config;
                 }))
+                .securityContextRepository(securityContextRepository)
                 .authorizeExchange(exchangeSpec -> exchangeSpec.pathMatchers("/product/api/v1/auth/**").permitAll()
                         .anyExchange().authenticated())
+                .addFilterAfter(jwtFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .logout(ServerHttpSecurity.LogoutSpec::disable)
