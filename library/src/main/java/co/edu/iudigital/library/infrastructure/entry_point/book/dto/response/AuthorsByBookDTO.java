891a92edd929b3307426f6c7ca744787b60d6f70
DarkDev <redprogramador@gmail.com>
2025-03-18 00:15:18 -0500

    update

---
+++ library/src/main/java/co/edu/iudigital/library/infrastructure/entry_point/book/dto/response/AuthorsByBookDTO.java
@@ -0,0 +1,6 @@
+package co.edu.iudigital.library.infrastructure.entry_point.book.dto;
+
+public record AuthorsByBook(Integer id,
+                            String firstName,
+                            String lastName) {
+}
