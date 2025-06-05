15d8b2fd9b9de2fa4f6d7a260ad822118950dbfe
DarkDev <redprogramador@gmail.com>
2025-06-04 20:16:07 -0500

    actalizar la lectura del id en request de updateBook

--- library/src/main/java/co/edu/iudigital/library/infrastructure/entry_point/book/BookHandler.java
+++ library/src/main/java/co/edu/iudigital/library/infrastructure/entry_point/book/BookHandler.java
@@ -151,7 +151,7 @@ public class BookHandler {
     public Mono<ServerResponse> updateBook(ServerRequest request) {
         return request.multipartData()
                 .flatMap(parts -> {
-                    int bookId = Integer.parseInt(request.pathVariable("id"));
+                    int bookId = Integer.parseInt(String.valueOf(request.queryParam("id")));
 
                     return Mono.zip(
                                     extractString(Objects.requireNonNull(parts.getFirst("title"), "The 'title' field is required")),
